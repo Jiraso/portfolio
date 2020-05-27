9893b00b4caa14d416607e233a39f74d71ea7998
jiraso <jamesian.r.solima@gmail.com>
2020-05-27 16:45:43 -0700

    Updates

---
+++ precache-manifest.92afe6602d3640292106d719ef3ba2d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "073c1f79bf3485d028445f2eda5d45f7",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "8598e0d40cb82fe7216b",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "ef7465a1f5d18df751fc",
+    "url": "/portfolio/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "8598e0d40cb82fe7216b",
+    "url": "/portfolio/static/js/2.56ef5511.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.56ef5511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef7465a1f5d18df751fc",
+    "url": "/portfolio/static/js/main.03f299bc.chunk.js"
+  },
+  {
+    "revision": "6f935e95a3f5e38874b6",
+    "url": "/portfolio/static/js/runtime-main.62b0c177.js"
+  }
+]);
