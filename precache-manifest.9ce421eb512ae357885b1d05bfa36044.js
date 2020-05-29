c403b5e67f2673ab17995a4e4f7612a02d554cb8
jiraso <jamesian.r.solima@gmail.com>
2020-05-28 17:49:40 -0700

    Updates

---
+++ precache-manifest.9ce421eb512ae357885b1d05bfa36044.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "615cad73800b393e61d26b43c71c4e11",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "713123047e5cdfdb07c1",
+    "url": "/portfolio/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "df2e2b4736c72ea2ccf0",
+    "url": "/portfolio/static/css/main.13f7c62a.chunk.css"
+  },
+  {
+    "revision": "713123047e5cdfdb07c1",
+    "url": "/portfolio/static/js/2.7171587b.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/portfolio/static/js/2.7171587b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "df2e2b4736c72ea2ccf0",
+    "url": "/portfolio/static/js/main.24dcd163.chunk.js"
+  },
+  {
+    "revision": "6f935e95a3f5e38874b6",
+    "url": "/portfolio/static/js/runtime-main.62b0c177.js"
+  },
+  {
+    "revision": "d3f36227a49bdc726452c98ce5b4cd88",
+    "url": "/portfolio/static/media/comingsoon.d3f36227.jpg"
+  },
+  {
+    "revision": "148bd23008008bdd220355d9cc0eb39b",
+    "url": "/portfolio/static/media/hublink.148bd230.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.92afe6602d3640292106d719ef3ba2d6.js"
+  "/portfolio/precache-manifest.9ce421eb512ae357885b1d05bfa36044.js"
 );
 
 self.addEventListener('message', (event) => {
