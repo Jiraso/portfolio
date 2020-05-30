858e65437ef5e8f3d8bbe985cb958d2c87ff7cad
jiraso <jamesian.r.solima@gmail.com>
2020-05-29 17:50:21 -0700

    0529 updates to portfolio and contact me page

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
-import { 
-    Form, 
+import {
+    Form,
     FormGroup,
     Label,
     Input,
-    Button } from 'reactstrap';
+    Button,
+    Container
+} from 'reactstrap';
 
 import Content from '../components/Content';
 import Hero from '../components/Hero';
 
 
 class ContactPage extends React.Component {
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state={
-            name:'',
+        this.state = {
+            name: '',
             email: '',
             message: '',
             disabled: false,
@@ -22,29 +24,29 @@ class ContactPage extends React.Component {
         }
     }
 
-    handleChange= e =>{
+    handleChange = e => {
         const target = e.target;
         const value = target.type === 'checkbox' ? target.checked : target.value;
         const name = target.name;
-        this.setState({ [name] :value })
+        this.setState({ [name]: value })
     }
 
-    handleSubmit= e =>{
+    handleSubmit = e => {
         e.preventDefault();
 
         this.setState({
-            disabled:true,
+            disabled: true,
             //put in a conditional statement here for if email is sent or not
             emailSent: false
         })
     }
 
-    render(){
-    return(
-        <React.Fragment>
-         <Hero title={this.props.title}></Hero>
-         <Content>
-                <Form onSubmit={this.handleSubmit}>
+    render() {
+        return (
+            <React.Fragment>
+                <Hero title={this.props.title}></Hero>
+                <Content>
+                    {/* <Form onSubmit={this.handleSubmit}>
                     <FormGroup>
                         <Label for="full-name">Full Name</Label>
                         <Input
@@ -86,11 +88,44 @@ class ContactPage extends React.Component {
                     </Button>
                     {this.state.emailSent === true && <p className="d-inline success-msg">Email Sent</p>}
                     {this.state.emailSent === false && <p className="d-inline err-msg">Email Not Sent</p>}
-                </Form>
+                </Form> */}
+
+                    <Container id="contact">
+                        <div id="contact-container">
+                            <a
+                                className="contact-details linkedin"
+                                rel="noopener noreferrer"
+                                href="https://www.linkedin.com/in/james-ian-solima/"
+                                target="_blank">
+                                <img alt='linkedin' src="https://img.icons8.com/ios/50/000000/linkedin.png" />
+                            </a>
+                            <a
+                                className="contact-details facebook"
+                                rel="noopener noreferrer"
+                                href="https://www.facebook.com/james.solima.3"
+                                target="_blank">
+                                <img alt='facebook' src="https://img.icons8.com/ios/50/000000/facebook.png" />
+                            </a>
+                            <a
+                                className="contact-details github"
+                                id="profile-link" rel="noopener noreferrer"
+                                href="https://github.com/Jiraso"
+                                target="_blank">
+                                <img alt='github' src="https://img.icons8.com/ios/50/000000/github.png" />
+                            </a>
+                            <a
+                                className="contact-details personal-email"
+                                rel="noopener noreferrer"
+                                href="mailto:jamesian.r.solima@gmail.com"
+                                target="_blank">
+                                <img alt='email' src="https://img.icons8.com/ios/50/000000/secured-letter.png" />
+                            </a>
+                        </div>
+                    </Container>
                 </Content>
-        </React.Fragment>
-    )
-}
+            </React.Fragment>
+        )
+    }
 }
 
 export default ContactPage
